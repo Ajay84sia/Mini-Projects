9a3454e946f21aa6af2cb24eea8d669fcf827be4
Ajay84sia <ajay9679339@gmail.com>
2023-09-15 20:30:57 +0530

    submit

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -43,37 +43,37 @@ const Projects = () => {
     {
       title: "URL Shortener",
       image:
-        "https://github-production-user-asset-6210df.s3.amazonaws.com/98752820/267646989-14bde366-56f6-4fdd-b71b-884cc375283f.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAIWNJYAX4CSVEH53A%2F20230913%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20230913T121401Z&X-Amz-Expires=300&X-Amz-Signature=7028c5722f4921885a59103b4ecfe7661dfd9cf9164b7bf1ee4c78b5ce2575db&X-Amz-SignedHeaders=host&actor_id=98752820&key_id=0&repo_id=690878901",
-      description: "This app helps you to transform your code effortlessly! Say goodbye to syntax errors with our Code Converter app. Convert, debug, and check quality of your code with just a click.",
-      techStacks: ["react", "javascript","node.js","express.js", "mongodb"],
-      repolink: "https://github.com/Ajay84sia/Mini-Projects/tree/main/codebase/code-converter",
+        "https://github-production-user-asset-6210df.s3.amazonaws.com/98752820/268304910-663e378c-a521-4ff0-b15d-80fcff2eea3c.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAIWNJYAX4CSVEH53A%2F20230915%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20230915T141050Z&X-Amz-Expires=300&X-Amz-Signature=a43d77caace164f2371dcc91c8a94c4ceb1878136d4a5d8f274125f02ecd3373&X-Amz-SignedHeaders=host&actor_id=98752820&key_id=0&repo_id=690878901",
+      description: "Your shortcut to shorter, smarter URLs! Simplify your links and boost engagement with our URL Shortener app. Say goodbye to long, website links and hello to sleek, shareable links in seconds.",
+      techStacks: ["react", "javascript","node.js", "mongodb"],
+      repolink: "https://github.com/Ajay84sia/Mini-Projects/tree/main/codebase/URL_Shortener",
       livelink: "https://slice-url.netlify.app/",
     },
     {
       title: "Weather App",
       image:
-        "https://github-production-user-asset-6210df.s3.amazonaws.com/98752820/267646989-14bde366-56f6-4fdd-b71b-884cc375283f.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAIWNJYAX4CSVEH53A%2F20230913%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20230913T121401Z&X-Amz-Expires=300&X-Amz-Signature=7028c5722f4921885a59103b4ecfe7661dfd9cf9164b7bf1ee4c78b5ce2575db&X-Amz-SignedHeaders=host&actor_id=98752820&key_id=0&repo_id=690878901",
-      description: "This app helps you to transform your code effortlessly! Say goodbye to syntax errors with our Code Converter app. Convert, debug, and check quality of your code with just a click.",
+        "https://github-production-user-asset-6210df.s3.amazonaws.com/98752820/268306048-5e3917f8-c269-44df-a89b-bc43319a6897.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAIWNJYAX4CSVEH53A%2F20230915%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20230915T141514Z&X-Amz-Expires=300&X-Amz-Signature=79eef5352f9ba776b8ffc5658b7308cee0bd0b145fc740390f14268e81ad902d&X-Amz-SignedHeaders=host&actor_id=98752820&key_id=0&repo_id=690878901",
+      description: "Your personal meteorologist in your pocket! Stay ahead of the forecast with our sleek Weather App, providing you with instant weather updates and insights at your fingertips.",
       techStacks: ["html", "bootstrap","javascript","rapid-API"],
-      repolink: "https://github.com/Ajay84sia/Mini-Projects/tree/main/codebase/code-converter",
+      repolink: "https://github.com/Ajay84sia/Mini-Projects/tree/main/codebase/WeatherApp",
       livelink: "https://weatheralertapp.netlify.app/",
     },
     {
       title: "BMI Calculator",
       image:
-        "https://github-production-user-asset-6210df.s3.amazonaws.com/98752820/267646989-14bde366-56f6-4fdd-b71b-884cc375283f.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAIWNJYAX4CSVEH53A%2F20230913%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20230913T121401Z&X-Amz-Expires=300&X-Amz-Signature=7028c5722f4921885a59103b4ecfe7661dfd9cf9164b7bf1ee4c78b5ce2575db&X-Amz-SignedHeaders=host&actor_id=98752820&key_id=0&repo_id=690878901",
-      description: "This app helps you to transform your code effortlessly! Say goodbye to syntax errors with our Code Converter app. Convert, debug, and check quality of your code with just a click.",
+        "https://github-production-user-asset-6210df.s3.amazonaws.com/98752820/268306671-2131b29b-e358-49c9-9caa-ab206ed092e5.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAIWNJYAX4CSVEH53A%2F20230915%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20230915T141713Z&X-Amz-Expires=300&X-Amz-Signature=265fef6f6e493b3fd0660d392989247f296fefd8a9a1d1c003b98bdd3c598345&X-Amz-SignedHeaders=host&actor_id=98752820&key_id=0&repo_id=690878901",
+      description: "Your ultimate BMI Calculator! Quickly assess your wellness and track your progress effortlessly with our user-friendly app. Your path to a healthier you starts here!",
       techStacks: ["angular", "typescript","tailwind CSS"],
-      repolink: "https://github.com/Ajay84sia/Mini-Projects/tree/main/codebase/code-converter",
+      repolink: "https://github.com/Ajay84sia/Mini-Projects/tree/main/codebase/BMI_Calculator",
       livelink: "https://slice-url.netlify.app/",
     },
     {
       title: "Todo App",
       image:
-        "https://github-production-user-asset-6210df.s3.amazonaws.com/98752820/267646989-14bde366-56f6-4fdd-b71b-884cc375283f.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAIWNJYAX4CSVEH53A%2F20230913%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20230913T121401Z&X-Amz-Expires=300&X-Amz-Signature=7028c5722f4921885a59103b4ecfe7661dfd9cf9164b7bf1ee4c78b5ce2575db&X-Amz-SignedHeaders=host&actor_id=98752820&key_id=0&repo_id=690878901",
-      description: "This app helps you to transform your code effortlessly! Say goodbye to syntax errors with our Code Converter app. Convert, debug, and check quality of your code with just a click.",
-      techStacks: ["angular", "typescript","tailwind CSS","JSON-server"],
-      repolink: "https://github.com/Ajay84sia/Mini-Projects/tree/main/codebase/code-converter",
+        "https://github-production-user-asset-6210df.s3.amazonaws.com/98752820/268307596-7effeb0f-03d6-48d6-923b-bd5a6acf430f.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAIWNJYAX4CSVEH53A%2F20230915%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20230915T142029Z&X-Amz-Expires=300&X-Amz-Signature=5f28dcf6ea4c4d5709a3bb949c458889e419b077bf31a4457f4ce48078c010f0&X-Amz-SignedHeaders=host&actor_id=98752820&key_id=0&repo_id=690878901",
+      description: "Start your daily tasks with our Todo App, your ultimate todo app. Streamline your to-dos, boost organization, and simplify life one task at a time. Effortlessly achieve your goals!",
+      techStacks: ["angular", "typescript","tailwind CSS"],
+      repolink: "https://github.com/Ajay84sia/Mini-Projects/tree/main/codebase/Todo%20App",
       livelink: "https://ajay84sia-angular-todos.netlify.app/",
     },
   ];
